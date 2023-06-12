603ce098040f0abef5c49dd7cfdac560c9854305
xiguan00yu <949734824@qq.com>
2023-06-12 14:26:35 +0800

    fix: windo defineProperty

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-(function() {
-  'use strict';
+(function () {
+  "use strict";
   try {
     window.$localStorage = window.localStorage;
     window.$sessionStorage = window.sessionStorage;
@@ -7,55 +7,64 @@
     // Test webstorage existence.
     if (!window.localStorage || !window.sessionStorage) throw "exception";
     // Test webstorage accessibility - Needed for Safari private browsing.
-    window.localStorage.setItem('storage_test', 1);
-    window.localStorage.removeItem('storage_test');
+    window.localStorage.setItem("storage_test", 1);
+    window.localStorage.removeItem("storage_test");
   } catch (e) {
-    (function() {
+    (function () {
       function createCookie(name, value, days) {
         var expires;
         if (days) {
           var date = new Date();
-          date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+          date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
           expires = "; expires=" + date.toGMTString();
         } else {
           expires = "";
         }
-        document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+        document.cookie =
+          encodeURIComponent(name) +
+          "=" +
+          encodeURIComponent(value) +
+          expires +
+          "; path=/";
       }
 
       function readCookie(name) {
-        var results = document.cookie.match('(^|;) ?'+encodeURIComponent(name)+'=([^;]*)(;|$)');
+        var results = document.cookie.match(
+          "(^|;) ?" + encodeURIComponent(name) + "=([^;]*)(;|$)"
+        );
         return results ? decodeURIComponent(results[2]) : null;
       }
 
       function Storage(type) {
         function setData(data) {
           data = JSON.stringify(data);
-          if (type === 'session') {
+          if (type === "session") {
             createCookie(getSessionName(), data);
           } else {
-            createCookie('localStorage', data, 365);
+            createCookie("localStorage", data, 365);
           }
         }
 
         function clearData() {
-          if (type === 'session') {
-            createCookie(getSessionName(), '');
+          if (type === "session") {
+            createCookie(getSessionName(), "");
           } else {
-            createCookie('localStorage', '', 365);
+            createCookie("localStorage", "", 365);
           }
         }
 
         function getData() {
-          var data = readCookie(type === 'session' ? getSessionName() : 'localStorage');
+          var data = readCookie(
+            type === "session" ? getSessionName() : "localStorage"
+          );
           return data ? JSON.parse(data) : {};
         }
 
         function getSessionName() {
           if (!window.name) {
-            window.name = Math.random()+'-'+(new Date().getTime());
+            window.name = Math.random() + "-" + new Date().getTime();
           }
-          return 'sessionStorage-' + window.name;
+          return "sessionStorage-" + window.name;
         }
 
         // Initialize if there's already data.
@@ -64,42 +73,43 @@
         var obj = {
           POLYFILLED: true,
           length: 0,
-          clear: function() {
+          clear: function () {
             data = {};
             this.length = 0;
             clearData();
           },
-          getItem: function(key) {
-            return Object.prototype.hasOwnProperty.call(data, key) ? data[key] : null;
+          getItem: function (key) {
+            return Object.prototype.hasOwnProperty.call(data, key)
+              ? data[key]
+              : null;
           },
-          key: function(i) {
+          key: function (i) {
             var ctr = 0;
             for (var k in data) {
-              if (ctr++ == i)
-                return k;
+              if (ctr++ == i) return k;
             }
             return null;
           },
-          removeItem: function(key) {
+          removeItem: function (key) {
             if (Object.prototype.hasOwnProperty.call(data, key)) {
               delete data[key];
               this.length--;
               setData(data);
             }
           },
-          setItem: function(key, value) {
+          setItem: function (key, value) {
             if (!Object.prototype.hasOwnProperty.call(data, key)) {
               this.length++;
             }
-            data[key] = ''+value;
+            data[key] = "" + value;
             setData(data);
-          }
+          },
         };
         return obj;
       }
 
-      var localStorage = new Storage('local');
-      var sessionStorage = new Storage('session');
+      var localStorage = new Storage("local");
+      var sessionStorage = new Storage("session");
 
       window.$localStorage = localStorage;
       window.$sessionStorage = sessionStorage;
@@ -107,12 +117,18 @@
       try {
         window.localStorage = localStorage;
         window.sessionStorage = sessionStorage;
-      } catch(e) {}
-      try {
         // For Safari private browsing need to also set the proto value.
         window.localStorage.__proto__ = localStorage;
         window.sessionStorage.__proto__ = sessionStorage;
-      } catch(e) {}
+      } catch (e) {}
+      try {
+        Object.defineProperty(window, "localStorage", {
+          value: localStorage,
+        });
+        Object.defineProperty(window, "sessionStorage", {
+          value: sessionStorage,
+        });
+      } catch (e) {}
     })();
   }
 })();
